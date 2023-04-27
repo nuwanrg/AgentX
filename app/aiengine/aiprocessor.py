f0a7d2545d4a8e1dc82c63e197c7ab97603d3d89
nuwanrg <nuwan_rajitha@outlook.com>
2023-04-27 17:51:49 +0800

    add monitor package with universal log file

--- app/aiengine/aiprocessor.py
+++ app/aiengine/aiprocessor.py
@@ -1,5 +1,9 @@
+import sys
+import os
 from .config.config import Config, check_openai_api_key
+from monitor.logs import logger
+from colorama import Fore, Style
 
 def process_message(data)->str:
-    print('AIEngine Invoked')
+    logger.typewriter_log( "Invoked AI Message Processor",)
     return 'WhatsApp response'
